5f4c9bcf53c71228e61b26ab57513d46a8c6254c
Miroslav <miroslav.hristov@abv.bg>
2026-07-14 19:51:53 +0300

    added nsg's

--- main.tf
+++ main.tf
@@ -8,15 +8,15 @@ resource "azurerm_resource_group" "appgrp" {
 }
 
 module "network" {
-  source              = "./modules/network"
-  vnet_name           = local.conf.vnet_name
-  vnet_address_space  = local.conf.vnet_address_space
-  resource_group_name = azurerm_resource_group.appgrp.name
-  location            = azurerm_resource_group.appgrp.location
-  subnets             = local.conf.subnets
+  source                  = "./modules/network"
+  vnet_name               = local.conf.vnet_name
+  vnet_address_space      = local.conf.vnet_address_space
+  resource_group_name     = azurerm_resource_group.appgrp.name
+  location                = azurerm_resource_group.appgrp.location
+  subnets                 = local.conf.subnets
   network_interfaces_name = local.conf.network_interfaces_name
-  public_ips          = local.conf.public_ips
-  nsg_name          = local.conf.nsg_name
+  public_ips              = local.conf.public_ips
+  network_security_groups = local.conf.network_security_groups
 }
 module "compute" {
   source = "./modules/compute"
--- modules/network/main.tf
+++ modules/network/main.tf
@@ -34,25 +34,39 @@ resource "azurerm_public_ip" "public_ip" {
 
 # NSGS
 resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-${var.resource_group_name}"
+  for_each = var.network_security_groups
+  name                = each.value.nsg_name
   location            = var.location
   resource_group_name = var.resource_group_name
+}
 
-    security_rule {
-        name                       = "Allow-SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+resource "azurerm_network_security_rule" "rules" {
+  for_each = {
+    for entry in flatten([
+      for nsg_key, nsg in var.network_security_groups : [
+        for rule in nsg.security_rules : {
+          nsg_key = nsg_key
+          rule    = rule
+        }
+      ]
+    ]) : "${entry.nsg_key}-${entry.rule.name}" => entry
+  }
+
+  name                        = each.value.rule.name
+  priority                    = each.value.rule.priority
+  direction                   = each.value.rule.direction
+  access                      = each.value.rule.access
+  protocol                    = each.value.rule.protocol
+  source_port_range           = each.value.rule.source_port_range
+  destination_port_range      = each.value.rule.destination_port_range
+  source_address_prefix       = each.value.rule.source_address_prefix
+  destination_address_prefix  = each.value.rule.destination_address_prefix
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg[each.value.nsg_key].name
 }
 
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
   for_each = var.network_interfaces_name
   network_interface_id      = azurerm_network_interface.nics[each.key].id
-  network_security_group_id = azurerm_network_security_group.nsg.id
+  network_security_group_id = azurerm_network_security_group.nsg[each.value.nsg_name].id
 }
